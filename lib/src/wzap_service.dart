81c984b007413804badac1a61a497b9b05224a4b
Guilherme Martins <guilherme.m.zety@gmail.com>
2023-08-31 09:11:42 -0300

    :construction: correções

--- lib/src/wzap_service.dart
+++ lib/src/wzap_service.dart
@@ -1,19 +1,21 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:wzap/core/app_api_path.dart';
 
 class WzapService {
   Future<String?> login({required String email, required String password}) async {
     try {
-      var response = await Dio().post(AppApiPath.login, data: {
-        'email': email,
-        'password': password,
-      });
-      if (response.statusCode == null) {
-        return null;
-      }
+      var response = await http.post(
+        Uri.parse(AppApiPath.login),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
 
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
-        return response.data['token'];
+      if (response.statusCode > 199 && response.statusCode < 300) {
+        return jsonDecode(response.body)['token'];
       }
     } catch (err) {
       print(err);
@@ -24,24 +26,15 @@ class WzapService {
 
   Future<bool> sendMessage({required String message, required String phone, required String token, required String instance}) async {
     try {
-      var response = await Dio().post(AppApiPath.send_message,
-          data: {
-            'instance': instance,
-            'message': message,
-            'phone_number': phone,
-          },
-          options: Options(
-            headers: {
-              'Content-type': 'application/json',
-              'Authorization': 'bearer $token',
-            },
-          ));
+      var response = await http.post(Uri.parse(AppApiPath.send_message), body: {
+        'instance': instance,
+        'message': message,
+        'phone_number': phone,
+      }, headers: {
+        'Authorization': 'bearer $token',
+      });
 
-      if (response.statusCode == null) {
-        return false;
-      }
-
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
+      if (response.statusCode > 199 && response.statusCode < 300) {
         return true;
       }
     } catch (err) {
