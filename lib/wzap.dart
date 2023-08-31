620b5c398c7350e2dc299153e061faf045e65529
Guilherme Martins <guilherme.m.zety@gmail.com>
2023-08-31 09:19:19 -0300

    :construction: correções

--- lib/wzap.dart
+++ lib/wzap.dart
@@ -1,16 +1,2 @@
-import 'package:wzap/src/wzap_messager.dart';
-
 export 'src/wzap_messager.dart';
 export 'src/wzap_service.dart';
-
-main() async {
-  await WzapMessager().configure(
-    instance: 'WZP5547992572818',
-    email: 'relacionamento@w2o.com.br',
-    password: 'password',
-  );
-  await WzapMessager().sendMessage(
-    message: 'Teste2',
-    phone: '5547992452912',
-  );
-}
