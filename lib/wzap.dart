e7060d339cd512b37a61407848b299162a4b4f77
Guilherme Martins <guilherme.m.zety@gmail.com>
2023-11-14 15:14:13 -0300

    :construction: subindo versão http

--- lib/wzap.dart
+++ lib/wzap.dart
@@ -1,27 +1,2 @@
-import 'package:wzap/wzap.dart';
-
 export 'src/wzap_messager.dart';
 export 'src/wzap_service.dart';
-
-main() async {
-  try {
-    await WzapMessager().configure(
-      instance: 'WZP5547992572818',
-      email: 'relacionamento@w2o.com.br',
-      password: 'password',
-    );
-    await WzapMessager().sendMessage(message: 'salve', phone: '5547992452912');
-  } catch (e) {
-    print(e);
-  }
-}
-
-class MessagerService {
-  static Future setInstance() async {
-    await WzapMessager().configure(
-      instance: 'WZP5547992572818',
-      email: 'relacionamento@w2o.com.br',
-      password: 'password',
-    );
-  }
-}
